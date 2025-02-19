45ad06bce292f7d957c95db94e8757e440132b32
Iain Summerlin <111240303+niceNiceiain2@users.noreply.github.com>
2025-02-19 15:54:25 -0600

    SigninController

---
+++ app/src/main/java/com/utsa/studyplanner/controller/SigninController.java
@@ -0,0 +1,36 @@
+package com.utsa.studyplanner.controller;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.utsa.studyplanner.MainActivity;
+
+public class SigninController implements View.OnClickListener {
+
+    /*
+     * Iain Summerlin - tea587
+     *
+     */
+    @Override
+    public void onClick(View view) {
+        Context context = view.getContext();
+        int duration = Toast.LENGTH_SHORT;
+        String username = MainActivity.username.getText().toString();
+        String password = MainActivity.password.getText().toString();
+
+        if (username.equals("iain")) {
+            Toast toast = Toast.makeText(context,  username, duration);
+            toast.show();
+
+            Intent intent = new Intent(context, MainActivity.class);
+            intent.putExtra("username", username);
+            context.startActivity(intent);
+        }
+        else {
+            Toast toast = Toast.makeText(context, "Error wrong user " + username, duration);
+            toast.show();
+        }
+    }
+}
