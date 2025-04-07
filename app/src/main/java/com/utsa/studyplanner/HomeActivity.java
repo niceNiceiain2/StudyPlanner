1ce38be5b8ae513650057df84c5ecb49493c72c0
Pictomike <mirodriguez1124@gmail.com>
2025-04-06 19:26:18 -0500

    Added a Calendar screen, Activities screen and a Profile screen

--- app/src/main/java/com/utsa/studyplanner/HomeActivity.java
+++ app/src/main/java/com/utsa/studyplanner/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.utsa.studyplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +13,32 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Welcome message setup
         String username = getIntent().getStringExtra("username");
         TextView welcomeText = findViewById(R.id.welcomeMessage);
         welcomeText.setText("Welcome, " + username + "!");
+
+        // Navigation buttons
+        ImageButton calendarButton = findViewById(R.id.calendarButton);
+        ImageButton activitiesButton = findViewById(R.id.activitiesButton);
+        ImageButton profileButton = findViewById(R.id.profileButton);
+
+        // Go to CalendarActivity
+        calendarButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, CalendarActivity.class);
+            startActivity(intent);
+        });
+
+        // Go to ActivitiesActivity
+        activitiesButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, ActivitiesActivity.class);
+            startActivity(intent);
+        });
+
+        // Go to ProfileActivity
+        profileButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        });
     }
 }
