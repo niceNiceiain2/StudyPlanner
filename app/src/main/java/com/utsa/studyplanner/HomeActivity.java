0f6a05a2eb737d8a72475bd47d9c8a9f33a42ebf
Iain Summerlin <111240303+niceNiceiain2@users.noreply.github.com>
2025-04-07 16:31:34 -0500

    Change test test 123 to Welcome.

--- app/src/main/java/com/utsa/studyplanner/HomeActivity.java
+++ app/src/main/java/com/utsa/studyplanner/HomeActivity.java
@@ -16,7 +16,7 @@ public class HomeActivity extends AppCompatActivity {
         // Welcome message setup
         String username = getIntent().getStringExtra("username");
         TextView welcomeText = findViewById(R.id.welcomeMessage);
-        welcomeText.setText("Test Test 123, " + username + "!");
+        welcomeText.setText("Welcome, " + username + "!");
 
         // Navigation buttons
         ImageButton calendarButton = findViewById(R.id.calendarButton);
